0b0cf1cd437e0d968eef86603d77924b655a983e
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2024-04-04 23:20:32 +0500

    add/some improvements to some sections

--- public/script.js
+++ public/script.js
@@ -30,3 +30,14 @@ var swiper = new Swiper('.mySwiper', {
     },
   },
 });
+
+var swiper = new Swiper('.mySwiper4', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'progressbar',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
