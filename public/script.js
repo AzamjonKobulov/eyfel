61e7193452754a69ea35f58a9b8db3085c17ff11
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2024-04-04 22:53:56 +0500

    add/some improvements to some sections

--- public/script.js
+++ public/script.js
@@ -2,35 +2,27 @@
 var swiper = new Swiper('.mySwiper', {
   slidesPerView: 1,
   spaceBetween: 16,
-  loop: true,
   navigation: {
     nextEl: '.next',
     prevEl: '.prev',
   },
+  loop: true,
   breakpoints: {
-    640: {
+    0: {
       slidesPerView: 2,
-      spaceBetween: 24,
+      spaceBetween: 16,
     },
     768: {
-      slidesPerView: 2,
-      spaceBetween: 32,
+      slidesPerView: 3,
+      spaceBetween: 24,
     },
     1024: {
-      slidesPerView: 2,
+      slidesPerView: 3,
       spaceBetween: 24,
     },
     1280: {
-      slidesPerView: 3,
-      spaceBetween: 24,
-    },
-    1440: {
-      slidesPerView: 3,
-      spaceBetween: 32,
-    },
-    1536: {
       slidesPerView: 4,
-      spaceBetween: 32,
+      spaceBetween: 30,
     },
     1720: {
       slidesPerView: 4,
