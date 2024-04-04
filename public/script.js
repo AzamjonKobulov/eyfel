432a10ccf778f0000f892876322cb9c68d47d040
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2024-04-05 02:02:07 +0500

    add/reviews page

--- public/script.js
+++ public/script.js
@@ -32,6 +32,8 @@ var swiper = new Swiper('.mySwiper', {
 });
 
 var swiper = new Swiper('.mySwiper4', {
+  slidesPerView: 1,
+  spaceBetween: 16,
   pagination: {
     el: '.swiper-pagination',
     type: 'progressbar',
@@ -39,5 +41,27 @@ var swiper = new Swiper('.mySwiper4', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 2.2,
+      spaceBetween: 24,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    1280: {
+      slidesPerView: 3.95,
+      spaceBetween: 24,
+    },
+    1720: {
+      slidesPerView: 3.95,
+      spaceBetween: 30,
+    },
   },
 });
